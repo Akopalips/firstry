936c927cf8adc43f5f6c46f73222171c7781f704
Akopalips <50484098+Akopalips@users.noreply.github.com>
2019-05-29 13:41:32 +0700

    Delete run_firstry.cmd

--- run_firstry.cmd
+++
@@ -1,3 +0,0 @@
-CHCP 866
-"C:\Users\Даниил Кондратьев\AppData\Local\Programs\Python\Python37\python" "C:\Users\Даниил Кондратьев\AppData\Local\Programs\Python\Python37\Scripts\firstry\manage.py" runserver
-pause
